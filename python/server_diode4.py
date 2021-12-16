e381352cf8de225cee41b632e81a6ca012ed4be6
jatiaultp <pauljatiault@gmail.com>
2021-12-16 12:10:29 +0100

    fix distance feature in server_diode4

--- python/server_diode4.py
+++ python/server_diode4.py
@@ -124,35 +124,21 @@ def web():
 
 @app.route("/distance")
 def distance():
- global  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(gpio_tri,GPIO.OUT)
+  GPIO.setup(gpio_echo,GPIO.IN)
 
-         gpio_tri = 21          # Entree Trig du HC-SR04 branchee au GPIO 21
-         gpio_echo  = 20         # Sortie Echo du HC-SR04 branchee au GPIO 20
+  GPIO.output(gpio_tri, True)
+  time.sleep(0.00001)
+  GPIO.output(gpio_tri, False)
 
-         GPIO.setup(gpio_tri,GPIO.OUT)
-         GPIO.setup(gpio_echo,GPIO.IN)
+  while GPIO.input(gpio_echo)==0:  ## Emission de l'ultrason
+    debutImpulsion = time.time()
 
-         GPIO.output(gpio_tri, False)
+  while GPIO.input(gpio_echo)==1:   ## Retour de l'Echo
+    finImpulsion = time.time()
 
-         repet = input("Entrez un nombre de repetitions de mesure : 1 ")
+  distance = round((finImpulsion - debutImpulsion) * 340 * 100 / 2, 1)  ## Vitesse du son = 340 m/s
 
-         for x in range(int(repet)):    # On prend la mesure "repet" fois
+  print(f"La distance est de : {str(distance)}cm")
 
-            time.sleep(1)       # On la prend toute les 1 seconde
-
-            GPIO.output(gpio_tri, True)
-            time.sleep(0.00001)
-            GPIO.output(gpio_tri, False)
-
-            while GPIO.input(gpio_echo)==0:  ## Emission de l'ultrason
-              debutImpulsion = time.time()
-
-            while GPIO.input(gpio_echo)==1:   ## Retour de l'Echo
-              finImpulsion = time.time()
-
-            distance = round((finImpulsion - debutImpulsion) * 340 * 100 / 2, 1)  ## Vitesse du son = 340 m/s
-
-            print(f"La distance est de : {str(distance)}cm")
-
-         GPIO.cleanup()
- return "<p>distance</p>"
+  return f"<p>distance {distance}</p>"
