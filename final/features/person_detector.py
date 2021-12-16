40ac94065647dcac10d6515be35fc56c4b548b3d
jatiaultp <96112116+jatiaultp@users.noreply.github.com>
2021-12-16 15:53:36 +0100

    frame whith person + datetime

--- final/features/person_detector.py
+++ final/features/person_detector.py
@@ -1,1 +1,16 @@
-# string.
+from datetime import datetime
+from time import sleep
+
+filmname="%m%d%Y %H%M%S.jpg"
+
+@app.route("/person")
+def person () :
+ while True
+  time.sleep(60)
+  cap = cv2.VideoCapture(0)
+  cap.set(3,640)
+  cap.set(4,480)
+  cv2.imwrite(filename, result)
+  return send_file(filename, mimetype='/home/pi/data/persons')
+
+
