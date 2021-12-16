fb3b27129cbd668d2557788675659c126114c3dc
jatiaultp <pauljatiault@gmail.com>
2021-12-16 10:07:37 +0100

    Add annotation when shoot

--- python/server_diode4.py
+++ python/server_diode4.py
@@ -3,8 +3,9 @@ from flask import send_file
 import RPi.GPIO as GPIO
 import threading
 import time
-from picamera import PiCamera
 from time import sleep
+import cv2
+
 
 is_blink_requested = False
 
@@ -28,6 +29,49 @@ channel = 4
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.OUT)
 
+
+# Image annotatrion
+
+#This is to pull the information about what each object is called
+classNames = []
+classFile = "/home/pi/tools/Object_Detection_Files/coco.names"
+with open(classFile,"rt") as f:
+    classNames = f.read().rstrip("\n").split("\n")
+
+#This is to pull the information about what each object should look like
+configPath = "/home/pi/tools/Object_Detection_Files/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightsPath = "/home/pi/tools/Object_Detection_Files/frozen_inference_graph.pb"
+
+#This is some set up values to get good results
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+#This is to set up what the drawn box size/colour is and the font/size/colour of the name tag and confidence label
+def getObjects(img, thres, nms, draw=True, objects=[]):
+    classIds, confs, bbox = net.detect(img,confThreshold=thres,nmsThreshold=nms)
+#Below has been commented out, if you want to print each sighting of an object to the console you can uncomment below
+#print(classIds,bbox)
+    if len(objects) == 0: objects = classNames
+    objectInfo =[]
+    if len(classIds) != 0:
+        for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+            className = classNames[classId - 1]
+            if className in objects:
+                objectInfo.append([box,className])
+                if (draw):
+                    cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+                    cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
+                    cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+                    cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+
+    return img,objectInfo
+
+
+
 app = Flask(__name__)
 
 @app.route("/on")
@@ -44,12 +88,13 @@ def off():
 
 @app.route("/shoot")
 def shoot():
-  filename = '/tmp/test.jpg'
-  try:
-    with PiCamera() as camera:
-        camera.capture(filename)
-  finally:
-    return send_file(filename, mimetype='image/jpg')
+  filename = "/home/pi/data/test_annotated.jpeg"
+
+  success, img = cap.read()
+  result, objectInfo = getObjects(img,0.45,0.2)
+  print(objectInfo)
+  cv2.imwrite(filename, result)
+  return send_file(filename, mimetype='image/jpg')
 
 
 @app.route("/")
@@ -65,4 +110,3 @@ def web():
   </body>
 </html>
   """
-
