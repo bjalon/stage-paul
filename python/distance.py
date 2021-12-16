752690bb8dba4962fe17722916434ef91ac4aaa1
jatiaultp <pauljatiault@gmail.com>
2021-12-16 11:16:05 +0100

    New route for calculate distances

--- python/distance.py
+++ python/distance.py
@@ -7,13 +7,13 @@ print( "+-----------------------------------------------------------+")
 print( "|   Mesure de distance par le capteur ultrasonore HC-SR04   |")
 print( "+-----------------------------------------------------------+")
 
-Trig = 21          # Entree Trig du HC-SR04 branchee au GPIO 21
-Echo = 20         # Sortie Echo du HC-SR04 branchee au GPIO 20
+gpio_tri = 21          # Entree Trig du HC-SR04 branchee au GPIO 21
+gpio_echo  = 20         # Sortie Echo du HC-SR04 branchee au GPIO 20
 
-GPIO.setup(Trig,GPIO.OUT)
-GPIO.setup(Echo,GPIO.IN)
+GPIO.setup(gpio_tri,GPIO.OUT)
+GPIO.setup(gpio_echo,GPIO.IN)
 
-GPIO.output(Trig, False)
+GPIO.output(gpio_tri, False)
 
 repet = input("Entrez un nombre de repetitions de mesure : ")
 
@@ -21,14 +21,14 @@ for x in range(int(repet)):    # On prend la mesure "repet" fois
 
    time.sleep(1)       # On la prend toute les 1 seconde
 
-   GPIO.output(Trig, True)
+   GPIO.output(gpio_tri, True)
    time.sleep(0.00001)
-   GPIO.output(Trig, False)
+   GPIO.output(gpio_tri, False)
 
-   while GPIO.input(Echo)==0:  ## Emission de l'ultrason
+   while GPIO.input(gpio_echo)==0:  ## Emission de l'ultrason
      debutImpulsion = time.time()
 
-   while GPIO.input(Echo)==1:   ## Retour de l'Echo
+   while GPIO.input(gpio_echo)==1:   ## Retour de l'Echo
      finImpulsion = time.time()
 
    distance = round((finImpulsion - debutImpulsion) * 340 * 100 / 2, 1)  ## Vitesse du son = 340 m/s
--- python/server_diode4.py
+++ python/server_diode4.py
@@ -7,6 +7,7 @@ from time import sleep
 import cv2
 
 
+
 is_blink_requested = False
 gpio_led = 4
 gpio_echo = 20 
@@ -120,3 +121,42 @@ def web():
   </body>
 </html>
   """
+
+ GPIO.setmode(GPIO.BCM)
+
+ gpio_tri = 21          # Entree Trig du HC-SR04 branchee au GPIO 21
+ gpio_echo  = 20         # Sortie Echo du HC-SR04 branchee au GPIO 20
+
+ GPIO.setup(gpio_tri,GPIO.OUT)
+ GPIO.setup(gpio_echo,GPIO.IN)
+
+ GPIO.output(gpio_tri, False)
+
+ repet = input("Entrez un nombre de repetitions de mesure : ")
+
+ for x in range(int(repet)):    # On prend la mesure "repet" fois
+
+    time.sleep(1)       # On la prend toute les 1 seconde
+
+    GPIO.output(gpio_tri, True)
+    time.sleep(0.00001)
+    GPIO.output(gpio_tri, False)
+
+    while GPIO.input(gpio_echo)==0:  ## Emission de l'ultrason
+      debutImpulsion = time.time()
+
+    while GPIO.input(gpio_echo)==1:   ## Retour de l'Echo
+      finImpulsion = time.time()
+
+    distance = round((finImpulsion - debutImpulsion) * 340 * 100 / 2, 1)  ## Vitesse du son = 340 m/s
+
+    print(f"La distance est de : {str(distance)}cm")
+
+ GPIO.cleanup()
+
+
+@app.route("/distance")
+def distance():
+  global is_blink_requested
+  is_blink_requested = True
+  return "<p>distance!</p>"
